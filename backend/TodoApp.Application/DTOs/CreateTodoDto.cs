3c562ce7b1311794d3666b859a56991b88135e7c
Gayashan Indrasankha <gayashanindrasankha@gmail.com>
2026-06-08 22:11:47 +0530

    Add DTOs for create, update, and retrieve

---
+++ backend/TodoApp.Application/DTOs/CreateTodoDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TodoApp.Domain.Entities;
+
+namespace TodoApp.Application.DTOs
+{
+    public class CreateTodoDto
+    {
+        public string Title { get; set; } = string.Empty;
+        public string? Description { get; set; } = string.Empty;
+        public TodoStatus Status { get; set; } = TodoStatus.Active;
+    }
+}
--- backend/TodoApp.Application/DTOs/UpdateTodoDto.cs
+++ backend/TodoApp.Application/DTOs/UpdateTodoDto.cs
@@ -1,21 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TodoApp.Domain.Entities;
 
-namespace TodoApp.Domain.Entities
+namespace TodoApp.Application.DTOs
 {
-    public enum TodoStatus
+    public class UpdateTodoDto
     {
-        Active,
-        Completed
-    }
-    public class Todo
-    {
-        public int Id { get; set; }
         public string Title { get; set; } = string.Empty;
         public string? Description { get; set; } = string.Empty;
         public TodoStatus status { get; set; } = TodoStatus.Active;
-        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-        
     }
 }
