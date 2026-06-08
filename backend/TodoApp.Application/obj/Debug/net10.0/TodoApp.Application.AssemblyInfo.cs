24007138c9e745e3ec9847bc5f661e8bbe39c941
Gayashan Indrasankha <gayashanindrasankha@gmail.com>
2026-06-08 19:23:32 +0530

    task object created

--- backend/TodoApp.Application/obj/Debug/net10.0/TodoApp.Application.AssemblyInfo.cs
+++ backend/TodoApp.Application/obj/Debug/net10.0/TodoApp.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApp.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6dd2e28b4bbac63afd47413f3d21d14df853a21f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a6acd9617119bf1bf4ed575f310c1a6e0d59eb9")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApp.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApp.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- backend/TodoApp.Domain/Entities/Todo.cs
+++ backend/TodoApp.Domain/Entities/Todo.cs
@@ -4,7 +4,17 @@ using System.Text;
 
 namespace TodoApp.Domain.Entities
 {
-    internal class Todo
+    public enum TodoStatus
     {
+        NotStarted,
+        InProgress,
+        Completed
+    }
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string? Description { get; set; } = string.Empty;
+        public TodoStatus status { get; set; } = TodoStatus.NotStarted;
     }
 }
